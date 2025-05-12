b41aeff2de1281c08f6aa7d51104aa189d804230
Dal S Garbuja Pun <7892627+dal-gp@users.noreply.github.com>
2025-05-12 11:46:52 +1000

    change wezterm font and font size

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,4 +4,8 @@ local wezterm = require("wezterm")
 -- This will hold the configuration
 local config = wezterm.config_builder()
 
+-- font and font size
+config.font = wezterm.font("FiraMono Nerd Font")
+config.font_size = 16
+
 return config
