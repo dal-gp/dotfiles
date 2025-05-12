75b30467e4189ca4f38a8693752c9dc09ad79e3d
Dal S Garbuja Pun <7892627+dal-gp@users.noreply.github.com>
2025-05-12 11:48:04 +1000

    hide wezterm tab and title bar

--- .wezterm.lua
+++ .wezterm.lua
@@ -8,4 +8,10 @@ local config = wezterm.config_builder()
 config.font = wezterm.font("FiraMono Nerd Font")
 config.font_size = 16
 
+-- hide title bar
+config.enable_tab_bar = false
+
+-- config.window_decorations = "TITLE | RESIZE"
+config.window_decorations = "RESIZE"
+
 return config
